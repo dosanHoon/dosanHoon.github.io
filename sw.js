4e1b066a23776ee68ee8670eb1a1bda1e6a6bd82
dosanHoon <hangru1106@gmail.com>
2026-03-25 01:13:35 +0000

    deploy: auto build from develop 835946ce0c9754ca00b14cc9819bf9a1ad9b503b

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3cbcc1a8fc91fb71674d.js"
+    "url": "webpack-runtime-42a0daa7d17cb2e4a4f4.js"
   },
   {
     "url": "app.8735867127ca1e3d1543.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d7295775c3d4dae8fc496559daa2a27e"
+    "revision": "3d260546a5368656860a7acde7984ec9"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
