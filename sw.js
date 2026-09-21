6b9b28e604b8a8e1425501992ee0595bfb4e729f
dosanHoon <hangru1106@gmail.com>
2026-09-21 06:30:25 +0000

    deploy: auto build from develop fa8f638aeba2e678ae1965ba297a52c89e7afe5e fa8f638aeba2e678ae1965ba297a52c89e7afe5e

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-af2f9baf399d50ab2bc6.js"
+    "url": "webpack-runtime-316f402e02658634f872.js"
   },
   {
     "url": "app.14ea3eb250dc2339abe3.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d2eed4c2baf2d70babdf293501f53932"
+    "revision": "c37ac6b6f9c77bb165a9bbdf1427fbf1"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
